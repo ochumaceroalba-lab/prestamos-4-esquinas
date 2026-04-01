1ade582732b06fed3be070cc56c776c5ab48abd4
ochumaceroalba-lab <Ochumaceroalba@gmail.com>
2026-04-01 13:54:20 -0400

    Arreglando modales y nombres de archivos para producción

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 1. Le decimos al código qué elementos del HTML vamos a usar
 const reproductor = document.getElementById('miVideo');
 const fuente = document.getElementById('fuenteVideo');
@@ -86,47 +85,3 @@ window.addEventListener("click", function(event) {
         document.body.style.overflow = "auto";
     }
 });
-=======
-// 1. Le decimos al código qué elementos del HTML vamos a usar
-const reproductor = document.getElementById('miVideo');
-const fuente = document.getElementById('fuenteVideo');
-const etiquetaTitulo = document.getElementById('tituloActual');
-
-// Lista exacta de tus 5 videos
-const listaDeVideos = [
-    { archivo: 'videos/video1presentacioncomprimido.webm', nombre: 'Presentación Institucional' },
-    { archivo: 'videos/pasos123.webm', nombre: 'Solicitud de Crédito' },
-    { archivo: 'videos/videoelectrodomesticos.webm', nombre: '' },
-   
-];
-
-// 3. Empezamos en el video 0 (el primero de la lista)
-let indiceActual = 0;
-
-reproductor.onended = function() {
-    indiceActual++; // Intentamos pasar al siguiente video
-
-    if (indiceActual < listaDeVideos.length) {
-        // --- CASO A: Aún hay videos en la lista ---
-        fuente.src = listaDeVideos[indiceActual].archivo;
-        etiquetaTitulo.innerText = "" + listaDeVideos[indiceActual].nombre;
-        
-        reproductor.load();
-        reproductor.play();
-    } else {
-        // --- FIN DE LA LISTA (Opción del usuario) ---
-        console.log("Secuencia terminada. Esperando al usuario.");
-        
-        indiceActual = 0; // Reseteamos al primer video
-        fuente.src = listaDeVideos[indiceActual].archivo;
-        etiquetaTitulo.innerText = "¡Has visto todo! Haz clic en Play para repetir.";
-        
-        reproductor.load(); 
-        // NO ponemos reproductor.play(), así se queda quieto con el poster inicial
-    }
-
-    
-};
-
-
->>>>>>> 7036d059d6af9877eec3de1ca4f1bb74f13f0ae5
